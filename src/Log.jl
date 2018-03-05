35de2b2e6cebc0d7ca05281b0c83c83d2ac5ac25
swiesend <sebastian@wiesendahl.de>
2018-03-05 14:35:30 +0100

    add NamedTuple as result

--- src/Log.jl
+++ src/Log.jl
@@ -1,4 +1,7 @@
 module Log
+
+using NamedTuples
+
 export split_overlapping
 
 function split_overlapping(text::Array{String}, selector::Regex)
@@ -6,8 +9,8 @@ function split_overlapping(text::Array{String}, selector::Regex)
     inverted_index = Dict{String,Vector{Tuple{Int64,Int64}}}()
 
     for (i, line) in enumerate(text)
-        m::RegexMatch = match(selector, line)
-        if !(isnull(m))
+        m = match(selector, line)
+        if typeof(m) != Void
             key = m.captures[1]
             if haskey(inverted_index, key)
                 push!(inverted_index[key], (i, m.offset))
@@ -18,18 +21,29 @@ function split_overlapping(text::Array{String}, selector::Regex)
         end
     end
 
-    @show inverted_index
-
     splitted = Dict{String,Array{String}}()
 
+    first_all = length(text)
+    last_all = 0
     for key in keys(inverted_index)
         entries = inverted_index[key]
         first::Int64 = entries[1][1]
         last::Int64 = entries[end][1]
         splitted[key] = text[first:last]
+
+        if first < first_all
+            first_all = first
+        end
+
+        if last > last_all
+            last_all = last
+        end
     end
 
-    splitted
+    prefix = text[1:first_all-1]
+    suffix = text[last_all+1:end]
+
+    @NT(prefix=prefix, splitted=splitted, suffix=suffix)
 end
 
 end # module Log
--- test/Log.jl
+++ test/Log.jl
@@ -1,5 +1,6 @@
 using LogClustering.Log
 
+# test with some dummy data
 text = [
     string(randstring(), " Prefix 'a' ", randstring()),
     string(randstring(), " Prefix 'b' ", randstring()),
@@ -15,7 +16,15 @@ expected = Dict(
     "b" => text[2:4],
     "c" => text[5:6],
 )
+assert(result.splitted == expected)
+assert(result.prefix == [])
+assert(result.suffix == [])
 
-assert(result == expected)
-
-readlines("")
+# test with a log file
+text = readlines("data/logs/2016-06-01_13-56-53_1273.log")
+result = Log.split_overlapping(text, r"Workflow \'(.*?)\'")
+for key in keys(result.splitted)
+    assert(length(result.splitted[key]) in [94, 40, 37, 55])
+end
+assert(length(result.prefix) == 729)
+assert(length(result.suffix) == 19)
