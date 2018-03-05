e8e1130696e56676f3d0234d49792db352bd75d2
swiesend <sebastian@wiesendahl.de>
2018-03-05 21:53:51 +0100

    remove parallel inverted index

--- src/Index.jl
+++ src/Index.jl
@@ -108,32 +108,4 @@ function invert(text::Array{Array{String}}, token::String)
     inverted_index
 end
 
-
-function pinvert(text::Array{String}, token::Regex)    
-
-    function match_key(t)
-        (l, line) = t
-        m = match(token, line)
-        if typeof(m) != Void
-            key = m.captures[1]
-            (key, l , m.offset)
-        end
-    end
-
-    entries = pmap(match_key, enumerate(text))
-    entries = filter!(e -> e != nothing, entries)
-    
-    inverted_index = Dict{String,Vector{Tuple{Int64,Int64}}}()
-    for (key, l, w) in entries
-        if haskey(inverted_index, key)
-            push!(inverted_index[key], (l,w))
-        else
-            inverted_index[key] = Vector{Tuple{Int64,Int64}}()
-            push!(inverted_index[key], (l,w))
-        end
-    end
-
-    inverted_index
-end
-
 end # module Index
