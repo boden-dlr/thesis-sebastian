bdf04b339da84d00da5d925234f208519c5512ec
swiesend <sebastian@wiesendahl.de>
2018-03-11 18:11:19 +0100

    update the playground

--- test/playground.jl
+++ test/playground.jl
@@ -1,3 +1,7 @@
+using Base.Test
+
+# ----------------------------------------------------------------------
+# Array vs. Vector test
 
 function push_array(a::Array, r)
     for i in r
@@ -32,21 +36,106 @@ reduce((p,t)-> p+t[2], 0.0, results_da) / length(results_da)
 reduce((p,t)-> p+t[2], 0.0, results_dv) / length(results_dv)
 
 # ------------------------------------------------------------------------------
+# Array vs. Tuple keys
+#
 using DataStructures
 
-da = Dict{Tuple,Tuple{Int64,UnitRange{Int64},Array{Int64,1}}}()
-da[(1,2,4)] = (1,1:1,Int64[])
-da[(1,)] = (1,1:1,Int64[])
-da[(1,2)] = (1,1:1,Int64[])
-da[(1,2,3)] = (1,1:1,Int64[])
-da[[1,5]] = (1,1:1,Int64[])
-da
-DataStructures.OrderedDict(sort(collect(da)))
+# make arrays comparable for sort
+function Base.isless(as::Array{Int64,1}, bs::Array{Int64,1})
+    A = length(as)
+    B = length(bs)
+    for i = 1:min(A,B)
+        if as[i] > bs[i]
+            return false
+        end
+    end
+    if A < B
+        return true
+    else
+        return false
+    end
+end
 
-da = Dict{Array,Tuple{Int64,UnitRange{Int64},Array{Int64,1}}}()
+@time Base.isless((1,2,3), (1,2,3))
+
+@time assert(isless(Int64[], [1]) == true)
+@time assert(isless([1,2], [1,2,3]) == true)
+@time assert(isless([1,2], [1,2,3,4,5]) == true)
+
+@time assert(isless([1,2], [1]) == false)
+@time assert(isless([1,2,3], [1,2]) == false)
+@time assert(isless([1,5], [1,2,3,4,5]) == false)
+
+r = 1:10000
+as = [i for i in 1:10000]
+bs = vcat([i for i in 1:10000],10001)
+test1 = Vector()
+for i in r
+    push!(test1, @timed isless(as, bs))
+end
+bs = [0]
+test2 = Vector()
+for i in r
+    push!(test2, @timed isless(as, bs))
+end
+reduce((p,t)-> p+t[2], 0.0, test1) / length(test1)
+reduce((p,t)-> p+t[2], 0.0, test2) / length(test2)
+
+reduce((p,t)-> p+t[3], 0.0, test1) / length(test1)
+reduce((p,t)-> p+t[3], 0.0, test2) / length(test2)
+
+
+dt = Dict{Tuple,Tuple{Int64,UnitRange{Int64},Array{Int64,1}}}()
+dt[(1,2,4)] = (1,1:1,Int64[])
+dt[(1,)] = (1,1:1,Int64[])
+dt[(1,2)] = (1,1:1,Int64[])
+dt[(1,2,3)] = (1,1:1,Int64[])
+# NOTE: it is not possible to add an array diectly
+# da[[1,5]] = (1,1:1,Int64[]) # ERROR: MethodError: Cannot `convert` an object of type Array{Int64,1} to an object of type Tuple
+dt
+
+da = Dict{Array{Int64,1},Tuple{Int64,UnitRange{Int64},Vector{Int64}}}()
 da[[1,2,4]] = (1,1:1,Int64[])
 da[[1,]] = (1,1:1,Int64[])
 da[[1,2]] = (1,1:1,Int64[])
 da[[1,2,3]] = (1,1:1,Int64[])
 da
-DataStructures.OrderedDict(sort(collect(da)))
+# sort(lt=myisless)
+# ERROR: MethodError: no method matching isless(::Array{Int64,1}, ::Array{Int64,1})
+
+dv = Dict{Vector{Int64},Tuple{Int64,UnitRange{Int64},Vector{Int64}}}()
+dv[[1,2,4]] = (1,1:1,Int64[])
+dv[[1,]] = (1,1:1,Int64[])
+dv[[1,2]] = (1,1:1,Int64[])
+dv[[1,2,3]] = (1,1:1,Int64[])
+dv
+
+@time DataStructures.OrderedDict(sort(dt))
+@time DataStructures.OrderedDict(sort(da))
+@time DataStructures.OrderedDict(sort(dv))
+
+# ------------------------------------------------------------------------------
+# Extend a Pattern and Array to Tuple
+
+a = [1,2,3]
+tuple(a...)
+# Extend tuple
+extended = tuple(a..., 4)
+extended = tuple(extended..., 5)
+
+function extend_tuple(itr, e)
+    tuple(itr..., e)
+end
+
+t = ()
+tup_t = @timed for i = 1:10000
+    t = extend_tuple(t, i)
+end
+
+v = Vector()
+vec_t = @timed for i = 1:10000
+    append!(v,i)
+end
+
+tup_t
+vec_t
