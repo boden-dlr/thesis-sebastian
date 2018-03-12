458a5340ecf517b6e6df23b336df407edbc7e284
swiesend <sebastian@wiesendahl.de>
2018-03-12 12:36:22 +0100

    add timing for test

--- test/Pattern.jl
+++ test/Pattern.jl
@@ -18,15 +18,15 @@ data = Int64[1,2,3,5,4,5,6,1,2,3,7,6,5,4,1,2]
 # ----------------------------------------------------------------------
 
 min_sup     = 1
-unique      = true
+unique      = false
 overlapping = false
 gap         = 0
 N = Int64
 
-# sequence = data
+sequence = data
 # sequence = rand(1:100, 5000)
-data = readcsv("data/kate/51750S_6154V_148N_3K_15E_1234seed_embedded_KATE_clustered_kmeans_51750P_300k.csv")
-sequence = map(n->convert(Int64,n), data[:,1])
+# data = readcsv("data/kate/51750S_6154V_148N_3K_15E_1234seed_embedded_KATE_clustered_kmeans_51750P_300k.csv")
+# sequence = map(n->convert(Int64,n), data[:,1])
 
 # TODO:NOTE: JULIA BUG in OrderedDict!
 # vertical = OrderedDict(sort(Index.invert(sequence)))
@@ -35,31 +35,41 @@ vertical = Index.invert(sequence)
 alphabet = sort(collect(keys(vertical)))
 vertical_pairs = Index.pairs(sequence, gap=gap)
 pairs = collect(keys(vertical_pairs))
-db = OrderedDict{Vector{N},Vector{Vector{N}}}()
-for (key,rs) in collect(vertical_pairs)
-    S = length(rs)
-    if S < min_sup
-        delete!(vertical_pairs, key)
-    else        
-        k = [e for e in key]
-        v = [[p[1],p[2]] for p in rs]
-        # @show key, rs, S, k, v
-        db[k] = v
+
+timing = Vector()
+for _ in 1:1
+    db = OrderedDict{Vector{N},Vector{Vector{N}}}()
+    for (key,rs) in collect(vertical_pairs)
+        S = length(rs)
+        if S < min_sup
+            delete!(vertical_pairs, key)
+        else        
+            k = [e for e in key]
+            v = [[p[1],p[2]] for p in rs]
+            # @show key, rs, S, k, v
+            db[k] = v
+        end
     end
+    patterns = collect(keys(db))
+
+    (_, t, bytes, gctime, memallocs) = @timed Pattern.grow_depth_first!(
+        db,
+        patterns,
+        sequence,
+        vertical,
+        alphabet;
+        min_sup = min_sup,
+        unique = unique,
+        overlapping = overlapping,
+        gap = gap
+    )
+    
+    push!(timing, (t,bytes))
 end
+avg_time  = reduce((p,t)-> p+t[1], 0.0, timing) / length(timing)
+avg_bytes = reduce((p,t)-> p+t[2], 0.0, timing) / length(timing)
+
 db
-patterns = collect(keys(db))
 
-@time Pattern.grow_depth_first!(
-    db,
-    patterns,
-    sequence,
-    vertical,
-    alphabet;
-    min_sup = min_sup,
-    unique = unique,
-    overlapping = overlapping,
-    gap = gap
-)
-
-sort(collect(keys(db)), rev=true)
+# collect(keys(db))
+# sort(collect(keys(db)), rev=true)
