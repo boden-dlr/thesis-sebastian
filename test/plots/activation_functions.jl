6d6d4501d873a2ca5afac001ff6d8352b2a2e72c
swiesend <sebastian@wiesendahl.de>
2018-07-02 10:41:29 +0200

    add savefig

--- test/plots/activation_functions.jl
+++ test/plots/activation_functions.jl
@@ -6,7 +6,7 @@ gr()
 
 x = linspace(-pi,pi,10000);
 
-plot(x, [
+p = plot(x, [
     identity,
     sigmoid,
     tanh,
@@ -17,22 +17,24 @@ plot(x, [
     # bent identity
     x -> ((sqrt(x^2+1)-1)/2)+x,
     # flipped cos (S. Wiesendahl)
-    x -> x <= 0 ? cos(x)-1 : 1-cos(x),
-    x -> x <= 0 ? exp(cos(x)-1)-1 : log(1-cos(x)+1),
+    # x -> x <= 0 ? cos(x)-1 : 1-cos(x),
+    # x -> x <= 0 ? exp(cos(x)-1)-1 : log(1-cos(x)+1),
     ],
     labels=[
-        "identity",
-        "logistic (sigmoid)",
-        "tanh",
-        "arctan",
-        "sin",
-        "ReLU",
-        "leaky ReLU",
-        "Bent identity",
-        "flipped cos",
-        "flipped exp cos",
+        "1. identity",
+        "2. logistic (sigmoid)",
+        "3. tanh",
+        "4. arctan",
+        "5. sin",
+        "6. ReLU",
+        "7. leaky ReLU",
+        "8. Bent identity",
+        # "flipped cos",
+        # "flipped exp cos",
         ],
     ylims = (-1.5,1.5),
     legend = :bottomright,
     style = :auto,
     line = 3)
+
+savefig("data/plots/activation_functions.png")
