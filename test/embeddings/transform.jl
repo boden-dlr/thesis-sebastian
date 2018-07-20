c1f5714e4aa57e32862e760db41becef5a573d48
swiesend <sebastian@wiesendahl.de>
2018-07-20 08:57:18 +0200

    add gensim test for Word2Vec and GloVe

---
+++ test/embeddings/transform.jl
@@ -0,0 +1,7 @@
+using PyCall
+@pyimport gensim
+
+# from gensim.scripts.glove2word2vec import glove2word2vec
+# glove_input_file = 'glove.6B.100d.txt'
+# word2vec_output_file = 'glove.6B.100d.txt.word2vec'
+# glove2word2vec(glove_input_file, word2vec_output_file)
