ec2bd94017e1c977bf9f8d14e6bd0094f6d3477f
swiesend <sebastian@wiesendahl.de>
2018-07-28 17:39:58 +0200

    add types to avoid type inference

--- src/pattern-mining/t-span.jl
+++ src/pattern-mining/t-span.jl
@@ -78,18 +78,30 @@ function s_concatenation!(
             end
             moBeta = range.start:t
             # M = { mo | if is mo subset of moBeta }
-            M = filter(x -> x != nothing, [
-                (
-                    mo.start >= moBeta.start && mo.stop <= moBeta.stop
-                ) ? mo : nothing
-                for mo in moSet[beta]])
+            # M = filter(x -> x != nothing, [
+            #     (
+            #         mo.start >= moBeta.start && mo.stop <= moBeta.stop
+            #     ) ? mo : nothing
+            #     for mo in moSet[beta]])
+            M = Vector{Vector{Int64}}()
+            for mo in moSet[beta]
+                if mo.start >= moBeta.start && mo.stop <= moBeta.stop
+                    push!(M,mo)
+                end
+            end
             if isempty(M)
                 # N = { mo | if moBeta is proper subset of mo }
-                N = filter(x -> x != nothing, [
-                    (
-                        moBeta.start > mo.start && moBeta.stop <= mo.stop
-                    ) ? mo : nothing
-                    for mo in moSet[beta]])
+                # N = filter(x -> x != nothing, [
+                #     (
+                #         moBeta.start > mo.start && moBeta.stop <= mo.stop
+                #     ) ? mo : nothing
+                #     for mo in moSet[beta]])
+                N = Vector{Vector{Int64}}()
+                for mo in moSet[beta]
+                    if moBeta.start > mo.start && moBeta.stop <= mo.stop
+                        push!(N,mo)
+                    end
+                end
                 if !isempty(N)
                     moSet[beta] = filter(r->!(r in N), moSet[beta])
                     push!(moSet[beta], moBeta)
@@ -101,6 +113,13 @@ function s_concatenation!(
                         if support(moSet, beta) >= min_sup && ewu(total_utility, utilities, beta, moSet) >= min_utililty
                             if utility(utilities, beta) >= min_utililty
                             # if relative_utility(total_utility, utilities, beta) >= min_utililty
+                                # u = utility(utilities, beta)
+                                # ru = relative_utility(total_utility, utilities, beta)
+                                # println(u)
+                                # println(ru)
+                                # println(min_utililty)
+                                # println((u >= min_utililty, ru >= min_utililty))
+                                # println()
                                 hueSet[beta] = moSet[beta] # sort(moSet[beta], by=r->r.start)
                             end
                 
@@ -136,25 +155,25 @@ end
 
 
 function t_span!(
-    sequence,
-    utilities,
-    alpha,
-    moSet,
-    hueSet,
-    mtd,
-    min_sup,
-    min_utililty,
-    total_utility,
-    SES)
+    sequence::Vector{Int64},
+    utilities::Vector{Float64},
+    prefix::Vector{Int64},
+    moSet::Dict{Vector{Int64},Vector{Intervall}},
+    hueSet::Dict{Vector{Int64},Vector{Intervall}}, # result set
+    mtd::Int64,
+    min_sup::Int64,
+    min_utililty::Float64,
+    total_utility::Float64,
+    SES::Vector{Int64})
 
 
-    if support(moSet, alpha) >= min_sup && iesc(total_utility, utilities, alpha, SES) >= min_utililty
-        # @show support(moSet, alpha)
-        # @show iesc(total_utility, utilities, alpha, SES)
+    if support(moSet, prefix) >= min_sup && iesc(total_utility, utilities, prefix, SES) >= min_utililty
+        # @show support(moSet, prefix)
+        # @show iesc(total_utility, utilities, prefix, SES)
         s_concatenation!(
             sequence,
             utilities,
-            alpha,
+            prefix,
             moSet,
             hueSet,
             mtd,
@@ -173,11 +192,11 @@ sequence = Int64[1,2,3,5,4,8,5,1,2,3,7,5,5,1,2,4]
 # utilities = Float64[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8]
 
 
-data = readdlm("data/embedding/playground/2018-07-25_assignments_and_reconstruction_error.csv")
-sequence = map(n->convert(Int64,n), data[:,1])
-utilities = map(n->convert(Float64,n), data[:,2])
-max_utilitly = maximum(utilities)
-utilities = map(u->(u/max_utilitly)^100, utilities)
+# data = readdlm("data/embedding/playground/2018-07-25_assignments_and_reconstruction_error.csv")
+# sequence = map(n->convert(Int64,n), data[:,1])
+# utilities = map(n->convert(Float64,n), data[:,2])
+# max_utilitly = maximum(utilities)
+# utilities = map(u->(u/max_utilitly)^100, utilities)
 
 # event = 702
 # occs = Vector{UnitRange{Int64}}()
@@ -191,15 +210,15 @@ utilities = map(u->(u/max_utilitly)^100, utilities)
 # moSet = Dict([event] => occs)
 
 
-# utilities = rand(maximum(sequence))
+utilities = rand(maximum(sequence))
 # utilities = fill(1.0, maximum(sequence))
 
 
 vertical = Index.invert(sequence)
 moSet = Dict(map(kv -> [kv[1]] => map(v -> v:v, kv[2]), collect(vertical)))
 hueSet = Dict{Vector{Int64},Vector{Intervall}}()
-max_duration = 1
-min_support  = 2600
+max_duration = 16
+min_support  = 1
 min_utililty = 0.000038764
 min_utililty = 0.00004945573
 tu = total_utility(sequence, utilities)
@@ -209,10 +228,12 @@ tu = total_utility(sequence, utilities)
 utilitly_correction = length(sequence) / (min(max_duration+1, length(sequence)))
 # min_utililty = 0.6
 
+using ProfileView
+Profile.clear()
 ts = Vector{Tuple{Vector{Int},Float64,Base.GC_Diff}}()
 for prefix in sort(collect(keys(moSet)), by=x->x[1])
     @show prefix
-    t = @timed @time t_span!(
+    t = @profile @timed t_span!(
         sequence,     # ES - event sequence
         utilities,    #
         prefix,       # alpha
@@ -225,6 +246,7 @@ for prefix in sort(collect(keys(moSet)), by=x->x[1])
         prefix)       # initial SES == prefix
     push!(ts, (prefix,t[2],t[5]))
 end
+ProfileView.view()
 #sort(collect(moSet), by=kv->length(kv[1]), rev=false)
 sort(collect(hueSet), by=kv->(length(kv[2]),length(kv[1])), rev=false)
 
@@ -241,3 +263,7 @@ mean(secs)
 maximum(secs)
 
 indmax(secs)
+
+while true
+    sleep(5)
+end
