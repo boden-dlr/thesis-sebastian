b0442461fc14de194316c78b30e9c1eb1834a074
swiesend <sebastian@wiesendahl.de>
2018-07-29 20:25:21 +0200

    show parallel computing examples..

--- test/playground/performance.jl
+++ test/playground/performance.jl
@@ -29,3 +29,42 @@ b
 
 @time c[end] = 6
 @timed for _ in 1:times c[end] = 6 end
+
+
+#
+# Parallel workers
+#
+
+if nprocs() == 1
+    addprocs(3)
+end
+
+function external()
+    1+1
+end
+
+@everywhere module M
+    function mutate!(d,p)
+        e = external()
+        d[p] = fill(p:p, p)
+    end
+end
+
+d = Dict{Int64,Vector{UnitRange{Int64}}}()
+ps = [1:10...]
+pmap( p -> M.mutate!(d,p), ps)
+
+
+# 
+# Multi-Threading
+# 
+
+# export JULIA_NUM_THREADS=4
+
+Threads.nthreads()
+
+a = zeros(10)
+Threads.@threads for i = 1:10
+    a[i] = Threads.threadid()
+end
+a
