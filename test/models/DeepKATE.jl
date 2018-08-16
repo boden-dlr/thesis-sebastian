896766f403a8835358ac2b7ee9ced0eb7b6f1c12
swiesend <sebastian@wiesendahl.de>
2018-08-16 22:27:20 +0200

    make deep-kate multi pareto

--- test/models/DeepKATE.jl
+++ test/models/DeepKATE.jl
@@ -6,53 +6,96 @@ using Flux: throttle, crossentropy
 
 function fit_deep_kate(data::Vector{Vector{Float64}}, seed::Integer, n_components=3, epochs=1)
     srand(seed)
-    Xs = data[1:end-1]
-    Ys = data[2:end]
+    Xs = data[1:end] # TODO/NOTE: no shift
+    Ys = data[1:end]
     N = length(data[1]) # is normalized line (max line length)
     L = n_components
-    activate = sin
+    # activate = sigmoid
+
+    lat = 4
 
     m = Chain(
         KATE.KCompetetive(N, 100, tanh, k=25),
-        Dense(100, 5, activate),
-        KATE.KCompetetive(5, L, tanh, k=L),
-        Dense(L, 5, activate),
-        Dense(5, 100, activate),
-        Dense(100, N, sigmoid)
+        # Dense(N, 100, sigmoid),
+        Dense(100, 5, sigmoid),
+        # KATE.KCompetetive(5, L, tanh, k=L), # round(Int,L/2)),
+        Dropout(0.25),
+        Dense(5, L, sin),
+        # softmax,
+        # softmax,
+        Dense(L, 5, sigmoid),
+        Dense(5, 100, sigmoid),
+        Dense(100, N, sigmoid),
         # KATE.KCompetetive(N, L, tanh, k=25),
-        # Dense(L, N, sigmoid)
+        # Dense(L, N, sigmoid),
     )
 
-    function loss(xs, ys)
-        ce = crossentropy(m(xs), ys)
-        Flux.truncate!(m)
-        ce
+    function loss(a,b,c)
+        # p,xs,n = triple(Xs)
+        # @show length(a),length(b),length(c)
+        
+        target_a = m[1:lat](a)
+        target_c = m[1:lat](c)
+
+        embedded = m[1:lat](b)
+        forward = m[lat+1:end](embedded)
+
+        prev = Flux.mse(embedded, 1.0 .- target_a)
+        succ = Flux.mse(embedded, 1.0 .- target_c)
+        # @show "test"
+        # prev = crossentropy(embedded, 1.0 .- target_a)
+        # succ = crossentropy(embedded, 1.0 .- target_c)
+        # prev = crossentropy(forward, 1.0 .- a)
+        ce = crossentropy(forward, b)
+        # succ = crossentropy(forward, 1.0 .- c)
+        # @show prev
+        # @show succ
+        # @show ce
+        # println()
+        # Flux.truncate!(m)
+        # 0.5*prev +
+        prev + ce + succ
     end
 
     function callback()
-        l = loss(Xs[5], Ys[5])
+        l = loss(Xs[4], Xs[5], Xs[6])
         println("loss:\t", l)
     end
 
     opt = Flux.ADAM(params(m))
 
+    # function triple(v)
+    #     n = length(v)
+    #     M = Matrix(n-2,3)
+    #     for i in 1:n-2
+    #         for j in 1:3
+    #             M[i,j] = v[i+j-1]
+    #         end
+    #     end
+    #     M
+    # end
+
     for e = 1:epochs
         info("Epoch $e")
         Flux.train!(
             loss,
-            zip(Xs, Ys),
+            zip(Xs[1:end-2],Xs[2:end-1],Xs[3:end]),
             opt,
             cb = throttle(callback, 5))
     end
 
+    for layer in m
+        if layer isa KATE.KCompetetive
+            layer.active = false
+        end
+    end
+     
     Flux.testmode!(m)
-    m[1].active = false
-    m[3].active = false
     m
 end
 
 function generate(model::Flux.Chain, data::Vector{Vector{Float64}})
-    embeddings = hcat(Flux.data.(model[1:3].(data))...)
+    embeddings = hcat(Flux.data.(model[1:4].(data))...)
     generated = hcat(Flux.data.(model.(data))...)
     embeddings', generated'
 end
