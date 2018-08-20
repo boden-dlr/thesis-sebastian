81ffad5cd30760031113efd3703d361b51bdac48
swiesend <sebastian@wiesendahl.de>
2018-08-20 13:10:33 +0200

    fix heatmap ticks

--- test/plots/heatmap.jl
+++ test/plots/heatmap.jl
@@ -6,9 +6,12 @@ gr()
 # pyplot()
 # plotlyjs()
 
-xs = [string("x",i) for i = 1:10]
-ys = [string("y",i) for i = 1:4]
-# z = float((1:4) * (1:10)')
+# cats = 16
+# xs = [string("x",i) for i = 1:cats]
+# ys = [string("y",i) for i = 1:cats]
+# z = float((1:cats) * (1:cats)')
+# p = heatmap(xs, ys, z)
+
 
 CVIs = CSV.read(joinpath(pwd(),"data/experiments/deep-kate/plot_cvi_heatmap_train=0.7_test=0.3_algo=auto_files=1048_1357_2407_5495_6073_6482.csv"))
 
@@ -23,10 +26,12 @@ p = heatmap(
     size=(800,800),
     aspect_ratio=1.0,
     xrotation=62,
-    tickfont = font(10),
+    # tickfont = font(5),
     # guidefont = font(8),
     # legendfont = font(8),
+    yticks = length(header),
+    xticks = length(header),
     )
 
 savefig(p,joinpath(pwd(),
-    "data/plots/cvi_heatmap_train=0.7_test=0.3_algo=auto_files=1048_1357_2407_5495_6073_6482.svg"))
+    "data/plots/cvi_heatmap_01.pdf"))
