66ca4631b024812eb63f7fdda9af053e4df4cb87
Sebastian Wiesendahl <sebastian@wiesendahl.de>
2018-08-30 10:16:08 +0200

    update project dependencies

---
+++ deps/build.jl
@@ -0,0 +1,13 @@
+
+# make project locally available
+# mkdir ~/.julia/config
+# touch ~/.julia/config/startup.jl
+# push!(LOAD_PATH, "path/to/local/julia/projects")
+
+# install python packages
+# Conda.add("scikit-learn")
+# Conda.add("ipykernel")
+
+# add current path to Python
+# ENV["PYTHON"] = "~/.julia/packages/Conda/???/deps/usr/bin"
+# build PyCall
