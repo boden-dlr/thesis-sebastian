f8a84629c7b8f0d5e631a3c300cf115f71b2d32b
Sebastian Wiesendahl <sebastian@wiesendahl.de>
2018-09-10 17:08:17 +0200

    fix typos

--- src/episode_mining/EpisodeMining.jl
+++ src/episode_mining/EpisodeMining.jl
@@ -1,26 +1,29 @@
 module EpisodeMining
 
-    # util functions: support, utility
+    # support- and utility-functions
     include("utility.jl")
-    # ms_span
-    export random_utility, local_utility, external_utility, avg_utility;
-    # mt_span
+    export random_utility;
+
+    # for mv_span
+    export local_utility, external_utility, avg_utility;
+
+    # for mt_span
     export support, relative_support;
     export utility, relative_utility, total_utility;
     export ewu, iesc;
 
-    # min sup mining
+    # frequent episode mining (FEM)
     include("recurring_growth.jl")
     export grow, mine_recurring
 
-    # HUEM
+    # high utility episode mining (HUEM)
     include("mv_span.jl")
     export mv_span, grow_depth_first!;
 
     include("mt_span.jl")
     export mt_span, s_concatenation;
 
-    # Rule Mining
+    # episode rule mining (ERM)
     include("rule_mining.jl")
 
 end # module
