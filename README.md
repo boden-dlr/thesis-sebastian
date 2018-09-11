83c98c3a37cb8fb3e60b36741f1fb81169eeae86
Sebastian Wiesendahl <sebastian@wiesendahl.de>
2018-09-11 22:01:21 +0200

    update build.jl and README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # LogClustering
+
+# Install
+
+# make the project locally available
+
+```sh
+mkdir ~/.julia/config
+touch ~/.julia/config/startup.jl
+```
+add to ~/.julia/config/startup.jl
+push!(LOAD_PATH, "/path/to/local/julia/reps")
+
+```pkg
+(v1.0) pkg> add /path/to/repository/LogClustering.jl
+```
+```pkg
+(v1.0) pkg> build LogClustering
+```
--- deps/build.jl
+++ deps/build.jl
@@ -1,13 +1,15 @@
-
 # make project locally available
 # mkdir ~/.julia/config
 # touch ~/.julia/config/startup.jl
 # push!(LOAD_PATH, "path/to/local/julia/projects")
 
 # install python packages
-# Conda.add("scikit-learn")
-# Conda.add("ipykernel")
+using Conda
+Conda.add("scikit-learn")
+#Conda.add("ipykernel")
+Conda.add("gensim")
 
 # add current path to Python
-# ENV["PYTHON"] = "~/.julia/packages/Conda/???/deps/usr/bin"
-# build PyCall
+#ENV["PYTHON"] = Conda.PYTHONDIR
+#using PyCall
+#build PyCall
